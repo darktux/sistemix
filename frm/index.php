30f00e8fad6cccaac05154c148e4e1216fc24e9f
romeoalexan777 <romeoalexan777@gmail.com>
2017-03-23 09:29:05 -0600

    actualizacion
    bootstrap-table
    index

--- frm/index.php
+++ frm/index.php
@@ -125,11 +125,9 @@
         <script src="../js/materialize.js"></script>
         <script src="../js/init.js"></script>
         <script type="text/javascript">
-            $( document ).ready()
-                $(function() {
-                    $("#formularios").load('html/Inicio.php');
-                }
-            );
+            $(document).ready(function() {
+                $("#formularios").load('html/Inicio.php');
+            });
             $("#opc-nper1").click(function(){
                 $("#formularios").load('html/Asociado.php');
             });
