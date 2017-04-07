60226b5114d213c55c7704c0e24939f5c97608ef
romeoalexan777 <romeoalexan777@gmail.com>
2017-04-07 02:45:46 -0600

    me
    cuenta,asociado

--- frm/html/Cuenta.php
+++ frm/html/Cuenta.php
@@ -1,30 +1,12 @@
 
 <!-- INICIA EL BLOQUE DEL MODAL -->
 <div class="fixed-action-btn">
-	<a class="modal-trigger btn-floating waves-effect waves-light btn-large cyan darken-1 tooltipped" href="#modal1" data-position="top" data-tooltip="Nueva cuenta"><i class="large material-icons">add</i></a>
+	<a class="modal-trigger btn-floating waves-effect waves-light btn-large cyan darken-1 tooltipped" href="#modal2" data-position="top" data-tooltip="Nueva cuenta"><i class="large material-icons">search</i></a>
 </div>
-
-
-
-
-
-
-
-
-
 <div id="modal1" class="modal modal-fixed-footer">
 	<form class="col s12" id="formulario" onsubmit="return submit1();">
 		<input id="idid" type="hidden" value="">
 		<div class="modal-content" id="modalcontent">
-
-
-
-
-
-
-
-
-
 			<h5>Nueva cuenta</h5><!-- //TITULO DEL MODAL *************************************************************************************************-->
 			<!-- INICIAN ELEMENTOS DEL FORMULARIO (CAMBIAR DEPENDIENDO DEL FORMULARIO A TRABAJAR) *****************************************************************-->
 			<div class="row">
@@ -70,11 +52,6 @@
 				</div>
 			</div>
 			<!-- TERMINAN ELEMENTOS DEL FORMULARIO ********************************************************************************************************************************************************-->
-
-
-
-
-
 		</div>
 		<div class="modal-footer">
 			<button class="waves-effect waves-light btn" type="submit">Guardar</button>
@@ -89,29 +66,11 @@
 	<div class="row">
 		<link href="../css/bootstrap-table.css" type="text/css" rel="stylesheet" media="screen,projection"/> 
 		<div class="col s12">
-
-
-
-
-
-
-
-
 	<!-- MODIFICAR LA data-url SEGUN SEA EL CASO DEL FORMULARIO QUE SE ESTE TRABAJANDO -->
 			<table id="tabla1" data-toggle="table" class="table table-striped table-hover"  data-url="php/Cuenta.php?acc=getjsontabla" data-click-to-select="true"  data-show-refresh="true" data-search="true" data-pagination="true" data-page-size="5" data-page-list="[5,8,10,20,50,100]">
-
-
-
-
-
 			    <thead>
 				    <tr>
 				    	<th data-field="operate" data-align="center" data-formatter="operateFormatter" data-events="operateEvents">Acciones</th>
-
-
-
-
-
 				    	<!-- INICIA ELEMENTOS DE LA TABLA (CAMBIAR DEPENDIENDO DEL FORMULARIO A TRABAJAR, USAR NOMBRES DE CAMPOS SEGUN BASE DE DATOS)*******************-->
 			            <th data-field="cuenta_monto" data-align="center">Monto</th>
 			            <th data-field="cuenta_fechaapertura" data-align="center">Fecha de apertura</th>
@@ -119,9 +78,6 @@
 			            <th data-field="cuenta_asociadoid" data-align="center">Asociado</th>
 			            <th data-field="cuenta_tipocuentaid" data-align="center">Tipo de cuenta</th>
 			            <!-- TERMINAN ELEMENTOS DE LA TABLA ************************************************************************************************************-->
-
-
-
 				    </tr>
 			    </thead>
 			</table>
@@ -130,14 +86,31 @@
 	</div>
 </div>
 <!-- TERMINA EL BLOQUE DE LA TABLA -->
+<div id="modal2" class="modal modal-fixed-footer">
+	<div class="modal-content" id="modalcontent">
+		<table id="tabla1" data-toggle="table" class="table table-striped table-hover"  data-url="php/Asociado.php?acc=getjsontabla" data-click-to-select="true"  data-show-refresh="true" data-search="true">
+		    <thead>
+			    <tr>
+			    	<th data-field="operate" data-align="center" data-formatter="operateFormatter1" data-events="operateEvents1">Acciones</th>
+			    	<!-- INICIA ELEMENTOS DE LA TABLA (CAMBIAR DEPENDIENDO DEL FORMULARIO A TRABAJAR, USAR NOMBRES DE CAMPOS SEGUN BASE DE DATOS)*******************-->
+		            <th data-field="asociado_nombre" data-align="center">Nombre</th>
+		            <th data-field="asociado_dui" data-align="center">DUI</th>
+		            <th data-field="asociado_nit" data-align="center">NIT</th>
+		            <!-- TERMINAN ELEMENTOS DE LA TABLA ************************************************************************************************************-->
+			    </tr>
+		    </thead>
+		</table>
+	</div>
+	<div class="modal-footer">
+		<button class="waves-effect waves-light btn" type="submit">Guardar</button>
+		<button class="modal-action modal-close waves-effect waves-light btn-flat" type="reset">Cancelar</button>
+	</div>
+</div>
 <!-- INICIA EL BLOQUE JAVASCRIPT -->
 <script type="text/javascript">
 /*INICIA FUNCION READY PARA INICIALIZAR LOS ELEMENTOS */
 	$(document).ready(function(){
-
-
 		$('#titu').hidden='false';
-
 	/*	$.ajax({
 	        type:"post",
 	        url: "php/Asociado.php",
@@ -157,7 +130,6 @@
         		$('#asoid').material_select();
 	        }
 	    });*/
-
 	    $.ajax({
 	        type:"post",
 	        url: "php/TipoCuenta.php",
@@ -175,29 +147,6 @@
         		$('#tipcueid').material_select();
 	        }
 	    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*INICIA BLOQUE DE CONFIGURACION DE VENTANA MODAL */
 		$('.modal').modal({
 			dismissible: true,
@@ -207,201 +156,18 @@
 			startingTop: '4%',
 			endingTop: '10%',
 			ready: function(modal, trigger){/*FUNCION QUE SE ACTIVA CUANDO SE ABRE EL MODAL */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 				document.getElementById("mon").focus();/*ID DEL PRIMER ELEMENTO DEL MODAL ****************************************************************************/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 				$('#modalcontent').animate({scrollTop:0},{duration:"slow"});
 			},
 			complete: function() {/*FUNCION QUE SE ACTIVA CUANDO SE CIERRA EL MODAL*/
 				$("#idid").val("");
 				$('label').removeClass("active");
-				
-		    	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 				//iNICIA BLOQUE PARA ANADIR CODIGO SEGUN SE NECESITE *************************************************************************************************
 				$("#asoid").val("");
 				$("#tipcueid").val("");
 		        $('select').material_select('destroy');
 		    	$('select').material_select();
 				//TERMINA BLOQUE ANADIR CODIGO SEGUN SE NECESITE******************************************************************************************************
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 			}
 		});
 /*FINALIZA BLOQUE DE CONFIGURACION DE VENTANA MODAL */
@@ -409,34 +175,6 @@
 /*FINALIZA FUNCION READY PARA INICIALIZAR LOS ELEMENTOS */
 /*INICIA FUNCION SUBMIT1 PARA GUARDAR Y MODIFICAR */
 	function submit1(){
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 		if($("#idid").val() == ""){/*DATOS PARA CREAR NUEVO REGISTRO (CAMBIAR DEPENDIENDO DEL FORMULARIO A TRABAJAR) *************************************************/
 	            datos = {
 	            	mon:$("#mon").val(),
@@ -459,32 +197,6 @@
 	            }
 		}
 		/*DATOS PARA MODIFICAR UN REGISTRO (FIN DEL BLOQUE) ********************************************************************************************************/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 		ejecutarajax(datos);
     	return false;
 	}
@@ -508,45 +220,6 @@
     window.operateEvents = {
 /*INICIA ACCION DEL BOTON MODIFICAR (COPIA LOS VALORES DEL REGISTRO A LOS CAMPOS DEL FORMULARIO MODAL)*/
         'click .edit': function (e, value, row, index) {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         	/*CAMBIAR SEGUN EL FORMULARIO QUE SE TRABAJA, LOS NOMBRES DE CAMPO DE row. SON COMO EN LA BASE DE DATOS***************************************************/
             $("#idid").val(JSON.stringify(row.cuenta_id).replace(/"/gi,''));
             $("#mon").val(JSON.stringify(row.cuenta_monto).replace(/"/gi,''));
@@ -555,87 +228,12 @@
             $("#asoid").val(JSON.stringify(row.cuenta_asociadoid).replace(/"/gi,''));
             $("#tipcueid").val(JSON.stringify(row.cuenta_tipocueid).replace(/"/gi,''));
         	/*CAMBIAR SEGUN EL FORMULARIO QUE SE TRABAJA, LOS NOMBRES DE CAMPO DE row. SON COMO EN LA BASE DE DATOS***************************************************/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         	$('label').addClass("active");
         	$('#modal1').modal('open');
         },
 /*TERMINA ACCION DEL BOTON MODIFICAR (COPIA LOS VALORES DEL REGISTRO A LOS CAMPOS DEL FORMULARIO MODAL)*/
 /*INICIA ACCION DEL BOTON ELIMINAR (COPIA LOS VALORES DEL REGISTRO A LOS CAMPOS DEL FORMULARIO MODAL)*/
         'click .remove': function (e, value, row, index) {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         	/*CAMBIAR SEGUN EL FORMULARIO QUE SE TRABAJA, LOS NOMBRES DE CAMPO DE row. SON COMO EN LA BASE DE DATOS***********************************************/
             if(confirm("Realmente desea eliminar el registro de la cuenta "+JSON.stringify(row.cuenta_monto).replace(/"/gi,''))){ /* CAMBIAR FRASE SEGUN LO QUE SE VA A ELIMINAR*/
             	datos = {
@@ -643,64 +241,13 @@
 	            	acc:'del'
 	            }
         	/*CAMBIAR SEGUN EL FORMULARIO QUE SE TRABAJA, LOS NOMBRES DE CAMPO DE row. SON COMO EN LA BASE DE DATOS***********************************************/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	            ejecutarajax(datos);
             }
         },
 /*TERMINA ACCION DEL BOTON ELIMINAR (COPIA LOS VALORES DEL REGISTRO A LOS CAMPOS DEL FORMULARIO MODAL)*/
 /*INICIA ACCION DEL BOTON VIEW (COPIA LOS VALORES DEL REGISTRO A LOS CAMPOS DEL FORMULARIO MODAL)*/
         'click .view': function (e, value, row, index) {
-
-
-
-
-
         	alert("sdfkhkj"); /********************************************************************************************************/
-
-
-
-
-
-            
         }
 /*TERMINA ACCION DEL BOTON VIEW (COPIA LOS VALORES DEL REGISTRO A LOS CAMPOS DEL FORMULARIO MODAL)*/
     };
@@ -708,206 +255,14 @@
 function ejecutarajax(datos){
 	$.ajax({
         type:"post",
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         url: "php/Cuenta.php",/*CAMBIAR LA RUTA DE ACUERDO AL FORMULARIO A TRABAJAR *****************************************************************************/
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         data:datos,
         success:function(responseText){
         	if(/Registro/.test(responseText)){
         		$('#modal1').modal('close');
         		alert(responseText);
         		$("#formulario")[0].reset();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         		$('#tabla1').bootstrapTable('refresh',{url:'php/Cuenta.php?acc=getjsontabla'});/*CAMBIAR LA RUTA DE ACUERDO AL FORMULARIO A TRABAJAR ************/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         		document.getElementById("buscar1").focus();
 				$('html,body').animate({scrollTop:$("#buscar1").offset().top},{duration:"slow"});
         	}
