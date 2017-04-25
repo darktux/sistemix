85dc7b291ff7325a04d5a00c8e9f072ed29f439d
Duglas Morales <mp.duglas@gmail.com>
2017-04-25 17:33:18 -0600

    changes
    Ya funciona la calculadora de credito, solo le faltan algunas
    validaciones. Cree en la base de datos una tabla suelta q solo uso para
    al macenar los calculos pero no afecta en nada ni se relaciona con
    nadie, solo es de soporte al proceso q hago.

--- frm/php/Capital.php
+++ frm/php/Capital.php
@@ -94,7 +94,69 @@
             else{
                 echo '0.00';
             }
-            break;           
+            break; 
+            case 'calc':
+                $i=0;
+                $salida=array();
+                $fila=array();
+                $tiempo=$_POST['tiempo'];
+                $monto=$_POST['monto'];
+                $interes=($_POST['interes']/12)/100;
+               
+                $constante=pow((1+$interes),$tiempo);
+                $arriba=$constante*$interes;
+                $abajo=$constante-1;
+                $cuota=round((($monto)*($arriba/$abajo)),2);
+                $con->consulta("DELETE FROM tab_calculo");
+
+                for($i=0;$i<$tiempo;$i++) {       
+                   
+                    if($cuota>$monto){
+                        $cuota=$monto+round($monto*$interes,2);
+                    }
+                    
+                    $amortizacion=round(($cuota-($monto*$interes)),2);
+                    $inter=round($monto*$interes,2);
+                    $monto=round($monto-$amortizacion,2);
+
+
+                    $fila[0]=$cuota;
+                    $fila[1]=$amortizacion;
+                    $fila[2]=$inter;
+                    $fila[3]=$monto;
+                     $con->consulta("INSERT INTO 
+                                tab_calculo (
+                                calculos_cuota,
+                                calculos_amortizacion,
+                                calculos_intereses,
+                                calculos_monto
+                            ) 
+                            VALUES(
+                                ".$cuota.",
+                                ".$amortizacion.",
+                                ".$inter.",
+                                ".$monto."
+                                );
+                        ");
+                   // $salida[$i]=$fila;
+                    //$i++;
+                }
+                //echo json_encode($salida);
+            break; 
+            case 'calcs':
+            $con->consulta("
+                SELECT 
+                    * 
+                FROM 
+                    tab_calculo
+            ");
+            $i=0;$salida=array();
+            while ($fila = mysql_fetch_array($con->getResultado(), MYSQL_ASSOC)) {       
+                $salida[$i]=$fila;
+                $i++;
+            }
+            echo json_encode($salida);
+            break;         
     }
 	//$con->limpiarConsulta();
     $con->desconectar();
--- frm/php/creaPDF.php
+++ frm/php/creaPDF.php
@@ -1,11 +1,12 @@
 <?php
     date_default_timezone_set('America/El_Salvador');
+    $loc_es=setlocale(LC_TIME,'es_ES.UTF-8','es_Es','es');
     include_once('pdf.php');
     include_once('Conex.php');
     $con= new Conex();
     $con->conectar();
 
-	$con->consulta("SELECT a.asociado_correlativo, a.asociado_nombre, a.asociado_profesionoficio, a.asociado_fechanacimiento, a.asociado_dui, a.asociado_departamento, a.asociado_municipio  FROM tab_asociado a, tab_cuenta b, tab_tipo_cuenta c where a.asociado_nombre='".$_GET['id']."'") and a.asociado_id=b.cuenta_idasociado and ;
+	$con->consulta("SELECT c.asociado_correlativo, c.asociado_nombre, c.asociado_profesionoficio, c.asociado_fechanacimiento, c.asociado_dui, c.asociado_departamento, c.asociado_municipio, a.tipocuenta_nombre FROM tab_tipo_cuenta a, tab_cuenta b, tab_asociado c where a.tipocuenta_id=b.cuenta_tipocuentaid and b.cuenta_asociadoid=c.asociado_id and c.asociado_nombre='".$_GET['id']."'");
     
     while ($row = mysql_fetch_array($con->getResultado(), MYSQL_ASSOC)){
 		$cod_per= $row['asociado_correlativo'];
@@ -15,9 +16,10 @@
 		$dui=$row['asociado_dui'];
 		$depto=$row['asociado_departamento'];
 		$muni=$row['asociado_municipio'];
+        $nomcta=$row['tipocuenta_nombre'];
     }
 
-    $fecha=CalculaEdad( $fec );
+    $fecha=CalculaEdad($fec);
 
     $html = '<b>La Asociacion Cooperativa de Ahorro y Credito de Empleados del Ministerio de Salud Region Paracentral de Responsabilidad Limitada</b>, que se abrevia <b>CO-AGESALUD DE R.L.</b>, con domicilio en Cojutepeque, departamento de Cuscatlan y <u>  '.$nom.'  </u>, de ocupacion <u>  '.$profe.'  </u>, de <u>  '.$fecha.'  </u> años de edad, con documento de identidad No. <u>  '.$dui.'  </u>, del domicilio de <u>  '.$muni.'  </u>, departamento de <u>  '.$depto.'  </u>, quienes en adelante de denominaran la Cooperativa y el Ahorrante, han convenido en celebrar el presente contrato, cuyas clausulas se detallan a continuacion: ';
     $html2='<br>1. Los retiros y depositos a la cuenta de ahorros, podran ser efectuados en agencias de la Cooperativa. <br>2. El (los) Ahorrante(s) designa(n) la(s) persona(s) autorizada(s) para retirar dinero de su cuenta, a las detalladas en el registro de firmas anexo a este contrato.<br>3. El(los) Ahorrante(s) designa(s) a los beneficiarios detallados en el registro de firmas anexo a este contrato y el porcentaje en que el saldo debera distribuirse, en caso que no existiere, se entendera que la distribución sera por partes iguales entre los beneficiarios.<br>4. Los depositos de ahorro no tendran limite y se haran en cantidades que el(los) ahorrante(s) abonen a su cuenta y devengaran intereses del <b>2.50% anual</b>, capitalizables al 31 de marzo, 30 de junio, 30 de septiembre y 31 de diciembre de cada a;o.';
@@ -25,7 +27,7 @@
     $html4='6. Los depositos efectuados con cheque a cargo de bancos locales o extranjeros y giros, quedaran sujetos al periodo de compensacion correspondiente y en caso de rechazo dentro o fuera de este periodo, se cargara a la cuenta de ahorros, el valor del cheque o giro, mas los costos, gastos, comisiones e impuestos que esto haya ocasionado.
 7. El(los) ahorrante(s) autoriza(n) a la Cooperativa, para que cargue en su cuenta de ahorros, las cantidades que le adeude(n), en concepto de intereses, prestamos, asi como tambien por cualquier clase de deudas y servicios que tuviese(n) pendiente de pago con la Cooperativa, mientras dure este contrato.
 8. La Cooperativa, podra cerrar la cuenta de ahorros en cualquier momento, por decision del Consejo de Administracion, avisando por escrito al ahorrante, abandonandole los intereses a la fecha de la clausura y deduciendo los saldos por deudas o servicios que estuviesen pendientes.'; 
-    if(strcmp($_GET['tipo_cuenta'], 'AHORRO NAVIDEÑO') == 0){
+    if(strcmp($nomcta, 'AHORRO NAVIDEÑO') == 0){
          $html3='5. La Cooperativa proporcionara al(los) ahorrante(s), libreta de ahorros, en la que se anotaran los depositos y retiros, asi como los intereses devengados, el ahorrante debera presentar la libreta cada vez que efectue operaciones de deposito y retiro de su cuenta de ahorros solo durante el mes de diciembre de cada año, condición especial de cuenta <b>AHORRO NAVIDEÑO</b>, con el fin de comprobar las operaciones y actualizaciones de su saldo.';
         $html5='9. Los menores de edad que hayan cumplido 16 años de edad podrán abrir cuentas de ahorros, efectuar depósitos y los retiros de su cuenta solo durante el mes de diciembre de cada año, condición especial de cuenta <b>AHORRO NAVIDEÑO</b>, libremente';
     }else
@@ -45,7 +47,7 @@
     $pdf->AddPage('P', 'Letter');
     
     $pdf->SetFont('Arial','B',8);
-     $pdf->Cell(0,-15,utf8_decode($_GET['tipo_cuenta']),0,1,'C'); 
+     $pdf->Cell(0,-15,utf8_decode($nomcta),0,1,'C'); 
    // $pdf->Ln(0.5);
     $pdf->Cell(0,25,'No. Cuenta: '.$_GET['cuenta'],0,1,'R'); 
    // $pdf->Ln(0.5);
@@ -60,15 +62,16 @@
 	$pdf->WriteHTML(utf8_decode($html));
 	$pdf->SetFontSize(6.5);
 	$pdf->WriteHTML($html2);
+    $pdf->Ln();
    // $pdf->WriteHTML($html3);
-    $pdf->MultiCell(0,5,utf8_decode($htm3));
+    $pdf->MultiCell(0,5,utf8_decode($html3));
     //$pdf->WriteHTML($html4);
     $pdf->MultiCell(0,5,utf8_decode($html4));
     //$pdf->WriteHTML($html5);
     $pdf->MultiCell(0,5,utf8_decode($html5));
     //$pdf->WriteHTML($html6);
     $pdf->MultiCell(0,5,utf8_decode($html6));
-    $pdf->Ln(10);
+    $pdf->Ln(5);
     $pdf->SetFont('Arial','B',8);
  	$pdf->Cell(0,0,'FIRMAS CONTRATANTES',0,1,'C'); 
     $pdf->Cell(0,30,'F.________________________________',0,1,'L'); 
@@ -78,16 +81,16 @@
     $pdf->Cell(160,25,'F.________________________________',0,1,'R'); 
     $pdf->Cell(160,-12,'AHORRANTE AUTORIZADO       ',0,1,'R');
     $pdf->SetFont('Arial','',6.5);
-    $pdf->Cell(0,0,utf8_decode('Autorización en la cuenta '),0,1,'L'); 
-    $pdf->Cell(0,10,'Nombre de la persona autorizada para tocar los fondos en esta',0,1,'L');
+    $pdf->Cell(0,-9    ,utf8_decode('Autorización en la cuenta '),0,1,'L'); 
+    $pdf->Cell(0,15,'Nombre de la persona autorizada para tocar los fondos en esta',0,1,'L');
     $pdf->Cell(0,0,'cuenta:_______________________________________________________',0,1,'L');
     $pdf->Cell(0,10,'#DUI:',0,1,'L');
     $pdf->Cell(90,-10,'#NIT:',0,1,'C');
     $pdf->Cell(45,10,'________________________',0,1,'C');
     $pdf->Cell(126,-10,' ________________________',0,1,'C');
-    $pdf->Cell(0,20,'Agencia: COJUTEPEQUE',0,1,'L');
+    $pdf->Cell(0,18,'Agencia: COJUTEPEQUE',0,1,'L');
     setlocale(LC_ALL, 'es_SV').': ';
-    $pdf->Cell(0,0,'LUGAR Y FECHA: Cojutepeque, '.iconv('ISO-8859-1', 'UTF-8', strftime('%A %d de %B de %Y', time())),0,1,'L');
+    $pdf->Cell(0,-10,'LUGAR Y FECHA: Cojutepeque, '.iconv('ISO-8859-1', 'UTF-8', strftime('%A %d de %B de %Y', time())),0,1,'L');
    
     
    
