bd7b4519edb49fa6f96b04586ebb46cd48994c94
romeoalexan777 <romeoalexan777@gmail.com>
2017-08-27 19:14:59 -0600

    print libreta usa post
    post en lugar de get

--- frm/php/printlibreta.php
+++ frm/php/printlibreta.php
@@ -16,30 +16,40 @@
 	    $printer = new Printer($connector);
 	    $printer -> setFont(Printer::FONT_B);
 	    $printer -> setJustification(Printer::JUSTIFY_RIGHT);
-
-	    
-
-	    $rs=mysql_query("SELECT cuentamovimiento_fecha, cuentamovimiento_comprobante, cuentamovimiento_retiro, cuentamovimiento_deposito, cuentamovimiento_saldo FROM tab_cuenta_movimiento WHERE cuentamovimiento_cuentaid='".$_GET['idcm']."'");
 	    $i=0;
+
+	    for($m=0;$m<$_POST['f'];$m++){
+	    	$printer -> text("\n");
+	    	$i++;
+	    }
+
+	    $rs=mysql_query("SELECT cuentamovimiento_fecha, cuentamovimiento_comprobante, cuentamovimiento_retiro, cuentamovimiento_deposito, cuentamovimiento_saldo, cuentamovimiento_id FROM tab_cuenta_movimiento WHERE cuentamovimiento_cuentaid='".$_POST['idcm']."' AND cuentamovimiento_impreso=0;");
+	    
 
 	    while($row = mysql_fetch_row($rs)) {
 	    	$i++;
-	        $fecha = trim($row[0]);
-	        $comprobante = trim($row[1]);
-	        $cargo = trim($row[2]);
-	        $abono = trim($row[3]);
-	        $saldo = trim($row[4]);
+	    	if($i<=57){
+		        $fecha = trim($row[0]);
+		        $comprobante = trim($row[1]);
+		        $cargo = trim($row[2]);
+		        $abono = trim($row[3]);
+		        $saldo = trim($row[4]);
 
-	        if($i<10){$ii="0".$i;}else{$ii=$i;}
+		        if($i<10){$ii="0".$i;}else{$ii=$i;}
 
-	        //$ii = str_pad($ii, 5, STR_PAD_LEFT);
-	        $fecha = str_pad($fecha, 13, " ", STR_PAD_LEFT);
-	        $comprobante = str_pad($comprobante, 18, " ", STR_PAD_LEFT);
-	        $cargo = str_pad($cargo, 17, " ", STR_PAD_LEFT);
-	        $abono = str_pad($abono, 17, " ", STR_PAD_LEFT);
-	        $saldo = str_pad($saldo, 17, " ", STR_PAD_LEFT);
+		        //$ii = str_pad($ii, 5, STR_PAD_LEFT);
+		        $fecha = str_pad($fecha, 13, " ", STR_PAD_LEFT);
+		        $comprobante = str_pad($comprobante, 18, " ", STR_PAD_LEFT);
+		        $cargo = str_pad($cargo, 17, " ", STR_PAD_LEFT);
+		        $abono = str_pad($abono, 17, " ", STR_PAD_LEFT);
+		        $saldo = str_pad($saldo, 17, " ", STR_PAD_LEFT);
 
-			$printer -> text( $ii.$fecha.$comprobante.$cargo.$abono.$saldo."\n");
+				$printer -> text( $ii.$fecha.$comprobante.$cargo.$abono.$saldo."\n");
+
+				mysql_query("UPDATE tab_cuenta_movimiento SET cuentamovimiento_impreso=1 WHERE cuentamovimiento_id=".$row[5].";");
+			}else{
+				break 1;
+			}
 
 	    }
     
